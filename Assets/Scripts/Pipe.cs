191e4e4776888b1f0765462bd3d7b75f8b016f15
perc123 <61948005+perc123@users.noreply.github.com>
2024-02-07 09:43:20 +0100

    Fixed Cube - Background Dimensions

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,28 +6,21 @@ public class Pipe : MonoBehaviour
     public GameObject pipePrefab; // Prefab of the pipe to be spawned
     private Color _color;
     void Start()
-    {
-        // Spawn the first pipe when the game starts
+    { 
         SpawnNewPipe();
     }
-
-
+    
     public void SpawnNewPipe()
     {
         // Randomly generate a position for the new pipe
         Vector3 newPosition = new Vector3(Random.Range(-15f, 15f), Random.Range(-15f, 15f), Random.Range(-15f, 15f));
-
-        //Random.InitState(System.DateTime.Now.Millisecond);
-
-        // Instantiate a new pipe at the random position
+        
         GameObject newPipe = Instantiate(pipePrefab, newPosition, Quaternion.identity);
-
-        _color = Random.ColorHSV();
+        
         // Set a random color for the new pipe
+        _color = Random.ColorHSV();
         newPipe.GetComponent<Renderer>().material.color = _color;
         Debug.Log(_color);
-        // Make the new pipe a child of the main pipe
-        //newPipe.transform.parent = transform;
     }
 
     public Color SetColor()
--- Assets/Scripts/RendomMovement.cs
+++ Assets/Scripts/RendomMovement.cs
@@ -1,120 +1,104 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class RandomMovement : MonoBehaviour
 {
-    public float speed = 5.0f; // Default speed value
-    private float rotationAngle = 90f; // Rotation angle in degrees
-
-    private float rotationTimer = 0f; // Timer to track the time elapsed since the last rotation
-    //private Pipe pipe;
+    public float speed = 10f; 
+    private float _rotationAngle = 90f; 
+    private float _rotationTimer; 
     
     [SerializeField] private GameObject mainObjectPrefab;
     [SerializeField] private GameObject cornerObjectPrefab;
-    private Color pipeColor;
+    private Color _pipeColor;
     [SerializeField] private GameObject pipePrefab;
 
-    private float objectLengthY; // Length of the object in the y-axis
-    private Vector3 previousPosition; // Previous position of the object
-    private bool mainObjectInstantiated = false; // Flag to track if the mainObject is instantiated
-    private bool isMoving = true; // Flag to indicate if the object is moving
-    private bool hasCollided = false;
+    private float _objectLengthY; // Length of the object in the y-axis - Body
+    private Vector3 _previousPosition; 
+    private bool _mainObjectInstantiated;
+    private bool _isMoving = true; 
+    private bool _hasCollided;
 
     private void Start()
     {
         // Get the length of the object in the y-axis
-        objectLengthY = mainObjectPrefab.GetComponent<Renderer>().bounds.size.y;
-        previousPosition = transform.position; // Store the initial position of the object
-        pipeColor = pipePrefab.GetComponent<Pipe>().SetColor();
-        gameObject.GetComponent<Renderer>().material.color = pipeColor;
-        //cornerObjectPrefab.GetComponent<Renderer>().material.color = pipePrefab.GetComponent<Pipe>().SetColor();
-
+        _objectLengthY = mainObjectPrefab.GetComponent<Renderer>().bounds.size.y;
+        _previousPosition = transform.position; // Store the initial position of the object
+        _pipeColor = pipePrefab.GetComponent<Pipe>().SetColor(); // Get the pipe color
+        gameObject.GetComponent<Renderer>().material.color = _pipeColor;
     }
 
     private void Update()
     {
-        if (isMoving)
+        if (_isMoving)
         {
             // Move the object forward towards the y-axis
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            transform.Translate(Vector3.up * (speed * Time.deltaTime));
 
             InstantiateMainObject();
 
-            // Update the rotation timer
-            rotationTimer += Time.deltaTime;
+            _rotationTimer += Time.deltaTime;
 
-            // Check if it's time to rotate the object
-            if (rotationTimer >= Random.Range(0.5f, 1.5f))
+            if (_rotationTimer >= Random.Range(0.5f, 1.5f))
             {
                 Rotate();
                 GameObject cornerObject =  Instantiate(cornerObjectPrefab, transform.position, transform.rotation);
-                cornerObject.GetComponent<Renderer>().material.color = pipeColor;
-                rotationTimer = 0f;
+                cornerObject.GetComponent<Renderer>().material.color = _pipeColor;
+                _rotationTimer = 0f;
             }
         }
     }
 
     private void InstantiateMainObject()
     {
-        // Check if the main object has not been instantiated and if the position exceeds its length
-        if (!mainObjectInstantiated &&
-            (Mathf.Abs(transform.position.x - previousPosition.x) >= objectLengthY*0.8 ||
-             Mathf.Abs(transform.position.y - previousPosition.y) >= objectLengthY*0.8 ||
-             Mathf.Abs(transform.position.z - previousPosition.z) >= objectLengthY*0.8))
+        if (!_mainObjectInstantiated &&
+            (Mathf.Abs(transform.position.x - _previousPosition.x) >= _objectLengthY*0.8 ||
+             Mathf.Abs(transform.position.y - _previousPosition.y) >= _objectLengthY*0.8 ||
+             Mathf.Abs(transform.position.z - _previousPosition.z) >= _objectLengthY*0.8))
         {
-            // Instantiate the main object
+            // Instantiate the main object - body
             GameObject mainObject = Instantiate(mainObjectPrefab, transform.position, transform.rotation);
 
-            // Set the color/material of the main object to match the pipe color
-            mainObject.GetComponent<Renderer>().material.color = pipeColor;
+            // Set the color
+            mainObject.GetComponent<Renderer>().material.color = _pipeColor;
 
-            // Set the flag to true
-            mainObjectInstantiated = true;
+            // Set the flag to true - ensure that it instantiates one time
+            _mainObjectInstantiated = true;
 
-            // Reset the previous position
-            previousPosition = transform.position;
+            _previousPosition = transform.position;
         }
-        else if (mainObjectInstantiated &&
-                 (Mathf.Abs(transform.position.x - previousPosition.x) < objectLengthY*0.8 &&
-                  Mathf.Abs(transform.position.y - previousPosition.y) < objectLengthY*0.8 &&
-                  Mathf.Abs(transform.position.z - previousPosition.z) < objectLengthY*0.8))
+        else if (_mainObjectInstantiated &&
+                 (Mathf.Abs(transform.position.x - _previousPosition.x) < _objectLengthY*0.8 &&
+                  Mathf.Abs(transform.position.y - _previousPosition.y) < _objectLengthY*0.8 &&
+                  Mathf.Abs(transform.position.z - _previousPosition.z) < _objectLengthY*0.8))
         {
-            // If the position is within the length, reset the flag
-            mainObjectInstantiated = false;
+            _mainObjectInstantiated = false;
         }
     }
 
 
     private void OnCollisionEnter()
     {
-        if (!hasCollided)
+        if (!_hasCollided)
         {
-            // Set the flag to true to indicate that the collision has been handled
-            hasCollided = true;
-            // Stop the movement of the collided pipe
+            // Indicate that the collision has been handled - ensure one collision per frame
+            _hasCollided = true;
             StopMovement();
             // Spawn a new pipe at a random position
             pipePrefab.GetComponent<Pipe>().SpawnNewPipe();
-            Debug.Log("collided");
         }
     }
 
     private void Rotate()
     {
-        // Get random rotation angle
-        rotationAngle = ChangeRotationAngle();
-        transform.Rotate(Vector3.forward, rotationAngle);
-        transform.Rotate(Vector3.right, rotationAngle);
+        _rotationAngle = ChangeRotationAngle();
+        transform.Rotate(Vector3.forward, _rotationAngle);
+        transform.Rotate(Vector3.right, _rotationAngle);
     }
 
-    // Method to randomly select either -90 or 90 degrees
     private float ChangeRotationAngle()
     {
-        // Generate a random integer between 0 and 2
         int randomNumber = Random.Range(0, 3);
 
-        // Map the random integer to one of the desired values
         switch (randomNumber)
         {
             case 0:
@@ -124,18 +108,17 @@ public class RandomMovement : MonoBehaviour
             case 2:
                 return -90;
             default:
-                return 0; // Default to 0 if unexpected value is generated
+                return 0;
         }
     }
 
     public void StopMovement()
     {
-        isMoving = false;
+        _isMoving = false;
     }
-
-
-
-
+    
+    
+    // TODO: - Half-size prefab to smoothen corner connections
     void EndingPrefab(Vector3 position, Quaternion rotation)
     {
         // Adjust the scale of the main object to half its length
@@ -143,7 +126,7 @@ public class RandomMovement : MonoBehaviour
         adjustedScale.y *= 0.5f;
 
         // Determine the axis the main object is moving on
-        Vector3 movementDirection = transform.forward; // Assuming the object moves along its forward direction
+        Vector3 movementDirection = transform.forward; // Assuming the object moves along its forward direction TODO: add other axis movement
         if (Mathf.Abs(Vector3.Dot(transform.right, movementDirection)) > 0.9f) // Check if moving mainly along the right axis
         {
             // Instantiate the main object with the adjusted scale at y + 0.5 on the right axis
@@ -165,9 +148,7 @@ public class RandomMovement : MonoBehaviour
             // Instantiate the main object with the adjusted scale at y + 0.5 on the forward axis
             GameObject mainObjectHalfLength = Instantiate(mainObjectPrefab, transform.position - Vector3.up * 0.5f, transform.rotation);
             mainObjectHalfLength.transform.localScale = adjustedScale;
-            Debug.Log("Here Ending3");
-
+            Debug.Log("Here Ending3"); 
         }
     }
-
 }
